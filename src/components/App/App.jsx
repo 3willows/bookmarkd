72e8ceb35b92c26e13df33c93b60c2eea58c80c6
Jasper Wong <wlcjasper@gmail.com>
2024-04-03 23:39:47 +0800

    start tinkering to make the interface work on my computer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,65 +1,66 @@
 /* eslint-disable no-inner-declarations */
-import Navbar from "../Navbar/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
-import { useEffect, useState, createContext } from "react";
-import { bookmarkd } from "../../definitions/bookmarkdTheme.jsx";
-import { ThemeProvider } from "@mui/material/styles";
-import { supabase } from "../Supabase/client.js";
-import AppRoutes from "../AppRoutes/AppRoutes.jsx";
-import DesktopNavbar from "../DesktopNavbar/DesktopNavbar.jsx";
+// import Navbar from "../Navbar/Navbar";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { useEffect, useState, createContext } from "react";
+// import { bookmarkd } from "../../definitions/bookmarkdTheme.jsx";
+// import { ThemeProvider } from "@mui/material/styles";
+// import { supabase } from "../Supabase/client.js";
+// import AppRoutes from "../AppRoutes/AppRoutes.jsx";
+// import DesktopNavbar from "../DesktopNavbar/DesktopNavbar.jsx";
 
-export const TokenContext = createContext();
-export const SetTokenContext = createContext();
+// export const TokenContext = createContext();
+// export const SetTokenContext = createContext();
 
 function App() {
-  const [token, setToken] = useState(false);
-  const [isMobile, setIsMobile] = useState(true);
+  // const [token, setToken] = useState(false);
+  // const [isMobile, setIsMobile] = useState(true);
 
-  function handleResize() {
-    const screenSize = window.innerWidth;
-    if (screenSize < 640) {
-      setIsMobile(true);
-    } else {
-      setIsMobile(false);
-    }
-  }
+  // function handleResize() {
+  //   const screenSize = window.innerWidth;
+  //   if (screenSize < 640) {
+  //     setIsMobile(true);
+  //   } else {
+  //     setIsMobile(false);
+  //   }
+  // }
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleResize);
 
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
+  //   // Call handler right away so state gets updated with initial window size
+  //   handleResize();
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
 
-  if (token) {
-    sessionStorage.setItem("token", JSON.stringify(token));
-  }
+  // if (token) {
+  //   sessionStorage.setItem("token", JSON.stringify(token));
+  // }
 
-  useEffect(() => {
-    if (sessionStorage.getItem("token")) {
-      let data = JSON.parse(sessionStorage.getItem("token"));
-      setToken(data);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (sessionStorage.getItem("token")) {
+  //     let data = JSON.parse(sessionStorage.getItem("token"));
+  //     setToken(data);
+  //   }
+  // }, []);
 
   return (
-    <TokenContext.Provider value={token}>
-      <SetTokenContext.Provider value={setToken}>
-        <ThemeProvider theme={bookmarkd}>
-          <Router>
-            {token && isMobile && <Navbar />}
-            {token && !isMobile && <DesktopNavbar />}
-            <div className="pb-16 lg:pl-[20rem] md:pl-[7.5rem]">
-              <AppRoutes token={token} />
-            </div>
-          </Router>
-        </ThemeProvider>
-      </SetTokenContext.Provider>
-    </TokenContext.Provider>
+    <div className="text-red">Hello</div>
+    // <TokenContext.Provider value={token}>
+    //   <SetTokenContext.Provider value={setToken}>
+    //     <ThemeProvider theme={bookmarkd}>
+    //       <Router>
+    //         {token && isMobile && <Navbar />}
+    //         {token && !isMobile && <DesktopNavbar />}
+    //         <div className="pb-16 lg:pl-[20rem] md:pl-[7.5rem]">
+    //           <AppRoutes token={token} />
+    //         </div>
+    //       </Router>
+    //     </ThemeProvider>
+    //   </SetTokenContext.Provider>
+    // </TokenContext.Provider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App/App.jsx";
-import "./index.css";
-import { StyledEngineProvider } from "@mui/material";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./components/App/App.jsx"
+import "./index.css"
+// import { StyledEngineProvider } from "@mui/material"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
+    {/* <StyledEngineProvider injectFirst> */}
+    <App />
+    {/* <>Hello</> */}
+    {/* </StyledEngineProvider> */}
   </React.StrictMode>
-);
+)
